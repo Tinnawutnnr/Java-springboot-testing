13cd8aef8f997b4d91aaa4d7b7086bc2da42f6e7
Tinnawutnnr <tinnawut.noin@kmutt.ac.th>
2025-01-24 22:14:14 +0700

    Update MappingController.java

--- src/main/java/com/example/training/MappingController.java
+++ src/main/java/com/example/training/MappingController.java
@@ -1,11 +1,25 @@
 package com.example.training;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MappingController {
 
+    @GetMapping("/hello-world")
     public String helloWorld() {
-        return "Hello World";
+        return "testing";
+    }
+
+    @PostMapping("/hello-world2")
+    public String helloWorld2() {
+        return "testing2";
+    }
+
+    @RequestMapping(path = "/hello-world3", method = {
+            RequestMethod.POST,
+            RequestMethod.GET
+    })
+    public String helloWorld3() {
+        return "testing3";
     }
 }
