985b99adce887f49f5247681a3c04fa8a2f86d39
Tinnawutnnr <tinnawut.noin@kmutt.ac.th>
2025-03-03 18:53:33 +0700

    Add JSON controller

--- src/main/java/com/example/training/JsonController.java
+++ src/main/java/com/example/training/JsonController.java
@@ -1,8 +1,7 @@
 package com.example.training;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class JsonController {
@@ -13,5 +12,30 @@ public class JsonController {
     ) {
         System.out.println("Success");
         return String.format("%s : %s", inputJsonFromClient.getUsername(), inputJsonFromClient.getPassword());
+    }
+
+    @GetMapping("test-JSON")
+    public UserData simpleReturn() {
+        UserData userData = new UserData();
+        userData.setFirstName("John");
+        userData.setLastName("Doe");
+        return userData;
+    }
+
+    @GetMapping("test-401")
+    public ResponseEntity<UserData> returnStatus401(
+            @RequestParam("input") String input
+    ) {
+        if("hacker".equals(input)) {
+            UserData userData = new UserData();
+            userData.setFirstName("Hacker");
+            userData.setLastName("Hackers");
+            return ResponseEntity.status(401).body(userData);
+        } else{
+            UserData userData = new UserData();
+            userData.setFirstName("John");
+            userData.setLastName("Doe");
+            return ResponseEntity.ok(userData);
+        }
     }
 }
---
+++ src/main/java/com/example/training/UserData.java
@@ -0,0 +1,25 @@
+package com.example.training;
+
+public class UserData {
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+}
